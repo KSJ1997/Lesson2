c1f19ef2dbb65edc1d6786981a02f22e269e1f01
SergeyKaraman <karamans7997@gmail.com>
2022-12-08 19:01:34 +0300

    Урок 2. Базовые алгоритмы
    Урок 2. Базовые алгоритмы

---
+++ Задача 10/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход 
+// трёхзначное число и на выходе показывает вторую цифру этого числа. 
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+Console.Write("Введи трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 100 && number <= 999)
+{
+    int lastDigit = number / 10 % 10;
+    int secondDigit = lastDigit % 10;
+    Console.WriteLine($"Вторая цифра числа {number} => {secondDigit}");
+}
+else Console.WriteLine("Число не трехзначное!");
---
+++ Задача 13/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 13: Напишите программу, которая выводит 
+// третью цифру заданного числа или сообщает, что третьей цифры нет. 
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Write("Введи число: ");
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+  if (anyNumber<100)
+    {
+        Console.WriteLine("третьей цифры нет");
+    }
+    else
+    {
+        while (anyNumber>999)
+        {
+            anyNumber /= 10;
+        }
+        Console.WriteLine($"третья цифра: {anyNumber%10} ");
+    }
